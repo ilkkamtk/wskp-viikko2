2282fdcbdca445c601a47106f7a0aafea5695af1
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2019-11-08 11:02:56 +0200

    pe

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -9,10 +9,14 @@ router.get('/', catController.cat_list_get);
 
 router.get('/:id', catController.cat_get);
 
-router.post('/',  upload.single('cat'), (req, res) => {
+router.post('/',  upload.single('cat'), (req, res, next) => {
   console.log('cat post file', req.file);
-  res.send('With this endpoint you can add cats.');
+  // tiedostonnimi bodyyn, jos haluaa
+  req.body.filename = req.file.filename;
+  next();
 });
+
+router.post('/', catController.cat_create_post);
 
 router.put('/', (req, res) => {
   res.send('With this endpoint you can edit cats.');
